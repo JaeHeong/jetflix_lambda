b465706513fcd76661db48a90f7743fe3d7c3798
jaehyeong <rlawogud970301@gmail.com>
2023-08-08 17:00:11 +0900

    Create get_all_list.py

---
+++ lambda/get_all_list.py
@@ -0,0 +1,54 @@
+import sys
+import logging
+import pymysql
+import json
+
+def lambda_handler(event, context):
+    rds_host = "db-test.cdiqjebgtx3e.ap-northeast-2.rds.amazonaws.com"
+    user_name = "admin"
+    password = "cloudneta"
+    db_name = "ASSETS"
+    
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    
+    try:
+        conn = pymysql.connect(
+            host=rds_host, user=user_name, passwd=password, db=db_name, connect_timeout=5
+        )
+        logger.info("연결 성공!")
+    except pymysql.MySQLError as e:
+        logger.error("연결 실패!")
+        logger.error(e)
+        sys.exit()
+        
+    cur = conn.cursor()
+
+    sql_insert = f"SELECT * FROM ASSET ORDER BY count DESC, title ASC;"
+    
+    cur.execute(sql_insert)
+
+    items = cur.fetchall()
+
+    json_array = []
+    
+    for item in items:
+        json_object = {
+        "id": item[0],
+        "title":item[1],
+        "overview":item[2]
+        }
+        json_array.append(json_object)
+
+    conn.close()
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(json_array),
+        "headers": {"Access-Control-Allow-Headers":
+                "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Methods":
+                "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+            "Access-Control-Allow-Origin":
+                "*"},
+    }
